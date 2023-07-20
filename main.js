24fdf3e20514fefb8619b49c3008b1e316fe73cb
daniel.varelas <daniel.varelas@estudiante.uam.es>
2023-07-20 23:58:49 +0200

    Rotating moon

--- main.js
+++ main.js
@@ -2,9 +2,78 @@ import * as THREE from 'three';
 import './style.css'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { gsap } from 'gsap';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+const canvas = document.querySelector('.webgl');
 const scene = new THREE.Scene();
 
+
+const loader = new GLTFLoader();
+loader.load( './assets/moon.glb', ( glb ) => {
+  console.log(glb);
+  const root = glb.scene;
+  root.scale.set(.0025, .0025, .0025);
+  scene.add(root);
+}, function (xhr){
+  console.log((xhr.loaded/xhr.total * 100) + "% loaded")
+}, function ( error ) {
+	console.error( error );
+} );
+
+
+const light = new THREE.PointLight(0xffffff, 1)
+light.position.set(0, 0, 3)
+scene.add(light)
+
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight, 
+}
+
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.set(0, 0, 3);
+scene.add(camera);
+ 
+
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.shadowMap.enabled = true
+renderer.gammaOutput = true;
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const loop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+}
+loop();
+animate()
+/*
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 
 const material = new THREE.MeshStandardMaterial({
@@ -88,4 +157,4 @@ window.addEventListener("mousemove", (e) => {
 
     console.log(newColor)
   }
-});
+});*/
