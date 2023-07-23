4fb621c12ecd48930f23eba928755374f77efbaa
daniel.varelas <daniel.varelas@estudiante.uam.es>
2023-07-23 23:44:01 +0200

    Version estable posicion luna

--- main.js
+++ main.js
@@ -6,17 +6,34 @@ import SunCalc from 'suncalc'; // Import the suncalc library
 import { gsap } from 'gsap';
 
 const canvas = document.querySelector('.webgl');
+const submitButton = document.querySelector('.submit-button');
+const datetimePicker = document.getElementById('datetimePicker');
+const panelContainer = document.querySelector('.panel-container');
+const purchaseButton = document.querySelector('.purchase-button');
+const cancelButton = document.querySelector('.cancel-button');
 const scene = new THREE.Scene();
 
-const light = new THREE.PointLight(0xffffff, 1);
-light.position.set(10, 10, 3);
+//SETUP
+
+
+//CREATE LIGHT
+const light = new THREE.DirectionalLight(0xffffff, 1); // Use DirectionalLight instead of PointLight
+light.position.set(-6, 0, 0); // Directional light doesn't need a position, but we keep it for reference
 scene.add(light);
 
+//LOAD MOON 3D
 const loader = new GLTFLoader();
+let moonModel; // Variable to hold the loaded moon 3D model
 loader.load('./assets/moon.glb', (glb) => {
-  const root = glb.scene;
-  root.scale.set(0.0025, 0.0025, 0.0025);
-  scene.add(root);
+  moonModel = glb.scene;
+  moonModel.scale.set(0.0025, 0.0025, 0.0025);
+  moonModel.rotation.set(-0.2, -0.3, 0.2);
+  scene.add(moonModel);
+  const latitude = 53;
+  const longitude = -2;
+  const date = new Date("2023-07-6"); // Use the selected date from the datetime picker
+  // Call the function to set the light to match the sun and move the camera to match the moon positions on the selected day
+  setMoonPhaseLight(date, latitude, longitude, moonModel);
 
 }, function (xhr) {
   console.log((xhr.loaded / xhr.total * 100) + '% loaded');
@@ -24,33 +41,56 @@ loader.load('./assets/moon.glb', (glb) => {
   console.error(error);
 });
 
+
+//SET WINDOW SIZES
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
 
+//CREATE CAMERA
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.set(0, 0, 3);
+camera.position.set(-3, 0, 0); // Directional light doesn't need a position, but we keep it for reference
 scene.add(camera);
 
+
+//RENDER CANVAS
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 5));
 renderer.shadowMap.enabled = true;
 renderer.gammaOutput = true;
 
+//ANIMATIONS
+
+//CONTROLS ON THE ORBIT
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = false;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.enableRotate = false;
+controls.autoRotate = true;
+//controls.autoRotateSpeed = 3;
+
+//CALL THE ANIMATION AND THE LOOP
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
 
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = false;
-controls.enablePan = false;
-controls.enableZoom = false;
-controls.autoRotate = true;
-controls.autoRotateSpeed = 3;
+const loop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+loop();
+animate();
 
+
+
+//EVENT LISTENERS
+
+//SIZES RECALCULATION
 window.addEventListener('resize', () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
@@ -60,95 +100,22 @@ window.addEventListener('resize', () => {
   renderer.setSize(sizes.width, sizes.height);
 });
 
-// Function to rotate the moon model to face the camera's target
-function rotateMoonModelToCameraTarget() {
-  const moonModel = scene.getObjectByName("moonModel"); // Assuming the moon model has a name "moonModel"
+//CANCEL INPUT DATE AND HIDE PANNEL
+cancelButton.addEventListener('click', () => {
+  panelContainer.classList.remove('show');
+  controls.autoRotate = true;
+  controls.autoRotateSpeed = 3;
 
-  if (moonModel) {
-    const target = new THREE.Vector3(); // Create a new target vector at the center of the scene (moon's position)
-    moonModel.getWorldPosition(target); // Get the current world position of the moon model
+  const selector = document.querySelector('.selector');
+  selector.classList.remove("topped");
+  const canv = document.querySelector('.webgl');
+  canv.style.left = "0%";
+  canv.style.top = "0%";
+  const tl = gsap.timeline({ defaults: { duration: 2 } });
+  tl.fromTo(".selector", { top:"20%" }, { top:"90%" });
+});
 
-    // Calculate the angle between the target vector and the moon model's position
-    const angle = Math.atan2(target.y - moonModel.position.y, target.x - moonModel.position.x);
-
-    // Apply the rotation to the moon model to face the camera's target
-    gsap.to(moonModel.rotation, {
-      z: angle,
-    });
-  }
-}
-
-// Update the camera's position and target based on the provided latitude and longitude
-function updateCameraPosition(latitude, longitude) {
-  const selectedDate = new Date(datetimePicker.value);
-  const moonPosition = SunCalc.getMoonPosition(selectedDate, latitude, longitude);
-
-  // Convert the moon's altitude and azimuth to radians
-  const altitude = THREE.MathUtils.degToRad(moonPosition.altitude);
-  const azimuth = THREE.MathUtils.degToRad(moonPosition.azimuth);
-
-  // Calculate the camera position based on the moon's position
-  const distance = 3; // Replace with the desired distance from the moon
-  const x = distance * Math.cos(altitude) * Math.cos(azimuth);
-  const y = distance * Math.cos(altitude) * Math.sin(azimuth);
-  const z = distance * Math.sin(altitude);
-
-  // Update the camera position and lookAt target
-
-  gsap.to(camera.position, {
-    x: x,
-    y: y,
-    z: z,
-  });
-  camera.position.set(x, y, z);
-  camera.lookAt(0, 0, 0); // Look at the center of the scene (moon's position)
-  rotateMoonModelToCameraTarget();
-}
-
-
-const submitButton = document.querySelector('.submit-button');
-const loop = () => {
-
-  const submitButton = document.querySelector('.submit-button');
-  // Update the camera position at each frame to match the selected date's coordinates
-
-  controls.update();
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(loop);
-};
-function updateLighting(date, latitude, longitude) {
-  // Calculate the sun's position based on the given date and location
-  const sunPosition = SunCalc.getPosition(date, latitude, longitude);
-  const moonPosition = SunCalc.getMoonPosition(date, latitude, longitude); // Calculate the moon's position
-
-  // Convert the sun's altitude and azimuth to radians
-  const altitude = THREE.MathUtils.degToRad(sunPosition.altitude);
-  const azimuth = THREE.MathUtils.degToRad(sunPosition.azimuth);
-
-  // Calculate the light direction from the altitude and azimuth
-  const lightDirection = new THREE.Vector3(
-    Math.cos(altitude) * Math.cos(azimuth),
-    Math.cos(altitude) * Math.sin(azimuth),
-    Math.sin(altitude)
-  );
-
-  // Adjust the light position based on the moon's position and direction vector
-  const moonPositionVector = new THREE.Vector3(moonPosition.azimuth, moonPosition.altitude, 0);
-  gsap.to(light.position, {
-    x: moonPositionVector.multiplyScalar(5).x,
-    y: moonPositionVector.multiplyScalar(5).y,
-    z: moonPositionVector.multiplyScalar(5).z,
-    duration: 1
-  });
-}
-
-loop();
-animate();
-
-
-
-const datetimePicker = document.getElementById('datetimePicker');
-
+//DATE HAS BEEN SET
 datetimePicker.addEventListener('input', () => {
   const selectedDate = new Date(datetimePicker.value);
 
@@ -165,133 +132,63 @@ datetimePicker.addEventListener('input', () => {
   }
 });
 
+//DATE HAS BEEN SUBMITED
 submitButton.addEventListener('mousedown', () => {
+  controls.enableRotate = false;
   controls.autoRotate = false;
   controls.autoRotateSpeed = 0;
-  updateLighting(new Date(datetimePicker.value), 40.46527931670784, -3.687180296631809);
-
-  
+  camera.position.set(-3, 0, 0)
 
   const selector = document.querySelector('.selector');
-  if (!selector.classList.contains("topped")){
+  if (!selector.classList.contains("topped")) {
     const canv = document.querySelector('.webgl');
     canv.style.left = "-30%";
     canv.style.top = "5%";
     const tl = gsap.timeline({ defaults: { duration: 2 } });
-    tl.fromTo(".selector", { top:"90%" }, { top:"20%" });
+    tl.fromTo(".selector", { top: "90%" }, { top: "20%" });
     selector.classList.add("topped");
   }
-  
-  const latitude = 40.46527931670784;
-  const longitude = -3.687180296631809; 
-  updateCameraPosition(latitude, longitude);
+
+  const latitude = 53;
+  const longitude = -2;
+  const date = new Date(datetimePicker.value); // Use the selected date from the datetime picker
+
 
   panelContainer.classList.add('show');
-})
 
-
-
-const panelContainer = document.querySelector('.panel-container');
-const purchaseButton = document.querySelector('.purchase-button');
-const cancelButton = document.querySelector('.cancel-button');
-
-
-cancelButton.addEventListener('click', () => {
-  panelContainer.classList.remove('show');
-  controls.autoRotate = true;
-  controls.autoRotateSpeed = 3;
-
-  const selector = document.querySelector('.selector');
-  selector.classList.remove("topped");
-  const canv = document.querySelector('.webgl');
-  canv.style.left = "0%";
-  canv.style.top = "0%";
-  const tl = gsap.timeline({ defaults: { duration: 2 } });
-  tl.fromTo(".selector", { top:"20%" }, { top:"90%" });
+  // Call the function to set the light to match the sun and move the camera to match the moon positions on the selected day
+  setMoonPhaseLight(date, latitude, longitude, moonModel);
 });
 
-/*
-const geometry = new THREE.SphereGeometry(3, 64, 64);
 
-const material = new THREE.MeshStandardMaterial({
-  color: "#00ff83",
-});
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// Sample function to set moon's visibility and illumination based on moon phase
+function setMoonPhaseLight(date, latitude, longitude, moonModel) {
 
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight, 
+  // Calculate the direction of the light (moon's position relative to the observer's position)
+  const moonPosition = SunCalc.getMoonPosition(date, latitude, longitude);
+  const lightDirection = new THREE.Vector3(
+    -Math.cos(moonPosition.azimuth) * Math.cos(moonPosition.altitude),
+    Math.sin(moonPosition.altitude),
+    Math.sin(moonPosition.azimuth) * Math.cos(moonPosition.altitude)
+  );
+  lightDirection.normalize();
+
+  // Calculate the angle to position the light to show the visible face of the moon
+  const moonToSunDirection = new THREE.Vector3().copy(lightDirection).negate();
+
+  // Create a plane using moonToSunDirection and the vertical plane normal
+  const planeNormal = new THREE.Vector3();
+  planeNormal.crossVectors(moonToSunDirection, new THREE.Vector3(0, 1, 0)).normalize();
+
+  // Calculate the position of the light (sun) based on the moon angle and fraction of illumination
+  const sunDistance = 6;
+  const lightPosition = new THREE.Vector3(
+    moonModel.position.x - moonToSunDirection.x * sunDistance,
+    moonModel.position.y - moonToSunDirection.y * sunDistance,
+    moonModel.position.z - moonToSunDirection.z * sunDistance
+  );
+
+  light.position.copy(lightPosition);
+
 }
-
-const light = new THREE.PointLight(0xffffff, 1, 100)
-light.position.set(0, 10, 10)
-scene.add(light)
-
-const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 100);
-camera.position.z = 20;
-scene.add(camera);
-
-
-const canvas = document.querySelector('.webgl');
-const renderer = new THREE.WebGLRenderer({ canvas });
-
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(2);
-renderer.render(scene, camera);
-
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-controls.enablePan = false;
-controls.enableZoom = false;
-controls.autoRotate = true;
-controls.autoRotateSpeed = 5;
-
-window.addEventListener('resize', () => {
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-  renderer.setSize(sizes.width, sizes.height);
-});
-
-const loop = () => {
-  controls.update();
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(loop);
-}
-loop();
-
-
-
-const tl = gsap.timeline({ defaults: { duration: 1 } });
-tl.fromTo(mesh.scale, { z:0, x:0, y:0 }, { z:1, x:1, y:1 });
-tl.fromTo("nav", { y:"-100%" }, { y:"0%" });
-tl.fromTo(".title", { opacity:0 }, { opacity:1 });
-
-let mouseDown = false;
-let rgb = [];
-window.addEventListener("mousedown", () => (mouseDown=true))
-window.addEventListener("mouseup", () => (mouseDown=false))
-
-window.addEventListener("mousemove", (e) => {
-  if (mouseDown) {
-    rgb = [
-      Math.round((e.pageX/sizes.width)*255),
-      Math.round((e.pageY/sizes.height)*255),
-      150,
-    ]
-
-
-    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
-    gsap.to(mesh.material.color, {
-      r: newColor.r,
-      g: newColor.g,
-      b: newColor.b,
-    })
-
-    console.log(newColor)
-  }
-});*/
